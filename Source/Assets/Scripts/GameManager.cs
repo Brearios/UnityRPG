0a98ae06d98252ab6d0bf8f56061e6a09628e3d3
Presin16 <presin16@gmail.com>
2019-05-05 22:50:30 -0500

    Initial Upload
    Build in the Build folder, source in the Source folder.

---
+++ Source/Assets/Scripts/GameManager.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if(GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            Destroy(player.gameObject);
+            Destroy(floatingTextManager.gameObject);
+            Destroy(hud);
+            Destroy(menu);
+            return;
+        }
+
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        // For scene transfers - replaced with a separate script DontDestroyOnLoad(gameObject);
+        // Lazy way to keep hitpoint bar working in other scenes DontDestroyOnLoad(hitpointBar.gameObject);
+    }
+
+    // Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    // References
+    public Player player;
+    public Weapon weapon;
+    public FloatingTextManager floatingTextManager;
+    public RectTransform hitpointBar;
+    public Animator deathMenuAnim;
+    public GameObject hud;
+    public GameObject menu;
+
+    // Logic
+    public int gold;
+    public int experience;
+
+    // Floating text
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
+
+    // XP testing - edit XP in game manager to make sure level goes up appropriately
+    // private void Update()
+    // {
+    //    Debug.Log(GetCurrentLevel());
+    // }
+
+    // Upgrade Weapon
+    public bool TryUpgradeWeapon()
+    {
+        // is the weapon max level?
+        if (weaponPrices.Count <= weapon.weaponLevel)
+            return false;
+        if (gold >= weaponPrices[weapon.weaponLevel])
+        {
+            gold -= weaponPrices[weapon.weaponLevel];
+            weapon.UpgradeWeapon();
+            return true;
+        }
+
+        return false;
+    }
+
+    // Hitboint Bar
+    public void OnHitpointChange()
+    {
+        float ration = (float)player.hitpoint / (float)player.maxHitpoint;
+        hitpointBar.localScale = new Vector3(1, ration, 1); // only modifying the Y (vertical) axis
+    }
+
+    // Experience system
+    public int GetCurrentLevel()
+    {
+        int r = 0;
+        int add = 0;
+
+        while (experience >= add)
+        {
+            add += xpTable[r];
+            r++;
+
+            if (r == xpTable.Count) // Max Level
+                return r;
+        }
+
+        return r;
+    }
+    public int GetXpToLevel(int level)
+    {
+        int r = 0;
+        int xp = 0;
+
+        while (r < level)
+        {
+            xp += xpTable[r];
+            r++;
+        }
+
+        return xp;
+    }
+    public void GrantXp(int xp)
+    {
+        int currLevel = GetCurrentLevel();
+        experience += xp;
+        if (currLevel < GetCurrentLevel())
+            OnLevelUp();
+    }
+    public void OnLevelUp()
+    {
+        Debug.Log ("Level Up!");
+        player.OnLevelUp();
+        OnHitpointChange();
+    }
+
+    // On Scene Loaded
+    public void OnSceneLoaded(Scene s, LoadSceneMode mode)
+    {
+        player.transform.position = GameObject.Find("SpawnPoint").transform.position;
+    }
+
+    // Death Menu and Respawn - moved to separate script
+    public void Respawn()
+    {
+        deathMenuAnim.SetTrigger("Hide");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+            player.Respawn();
+    }
+
+    // Save state
+    /*
+     * INT preferredSkin
+     * INT gold
+     * INT experience
+     * INT weaponLevel
+     */
+    public void SaveState()
+    {
+        string s = "";
+
+        s += "0" + "|"; 
+        s += gold.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += weapon.weaponLevel.ToString();
+
+        PlayerPrefs.SetString("SaveState", s);
+        // alt+124 is pipe character, which apparently adjoins the next line - hence the last line doesn't need it
+    }
+
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= LoadState;
+
+        if (!PlayerPrefs.HasKey("SaveState"))
+            return;
+    
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        // Change player skin
+        gold = int.Parse(data[1]);
+
+        // Experience
+        experience = int.Parse(data[2]);
+        if(GetCurrentLevel() !=1)
+            player.SetLevel(GetCurrentLevel());
+
+        // Change the weapon level
+        weapon.SetWeaponLevel(int.Parse(data[3]));
+
+        // Removed loadstate on scene transition, so unneeded: player.transform.position = GameObject.Find("SpawnPoint").transform.position;     
+    }
+}
